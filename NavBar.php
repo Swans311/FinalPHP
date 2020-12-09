ab9b622eec2691b2c488ec7c1b838d86678ab0dc
Casey Viens <casey.viens@gmail.com>
2020-12-09 00:13:30 -0500

    Added an img folder, and worked on setting up the html/css for the navbar

---
+++ NavBar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark text-white shadow p-3 mb-5"style="background-color: #448a9a;">
+    <a class="navbar-brand" href="#">Project Name/Logo</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="flex-grow:0">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Review</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+      </ul>
+    </div>
+    <form class="navbar-form mx-auto d-flex">
+      <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn" type="submit"><img src="misc\img\search_24.svg" alt="Search Icon"><img/></button>
+    </form>
+    <div class="nav-item ml-auto">
+      <p class="mb-0">test</p>
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+  <div style="height: 2000px"></div>
+</body>
+</html>
