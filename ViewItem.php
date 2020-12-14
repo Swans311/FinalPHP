73d1d6f61c050a1af32dc055b8db83714eadfe33
Casey Viens <casey.viens@gmail.com>
2020-12-13 19:33:28 -0500

    Created the ViewItem and ViewRestaurant pages while also setting up the start of the AboutUs page, additionally I created the images folder with some test images

---
+++ ViewItem.php
@@ -0,0 +1,74 @@
+<?php 
+    include(__DIR__.'/NavBar.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gourmandize | Food Item</title>
+</head>
+<body>
+<div class="container gz-div-glow">
+        <div class="container gz-div-inner mx-auto text-left py-5 text-white" style="font-family: textFont;">
+            <div class="media mr-auto mb-5">
+                <img class="mr-3 align-self-center" style="height: 300px; width: auto;" src="misc\images\Fries_Test.jpeg" alt="img">
+                <div class="media-body">
+                    <h1 class="display-4"style="font-family: titleFont;">Food Item Name</h1>
+                    <h1 class="display-4"style="font-family: titleFont;">Restaurant Name</h1>
+                    <h1 class="display-4">Categories</h1>
+                    <h1 class="display-4">Stars</h1>
+                </div>
+            </div>
+            <div class="row border border-white rounded m-2">
+                <div class="media mx-3" style="padding-top: 15px; padding-bottom: 15px;">
+                    <img class="mr-3 align-self-center" style="height: auto; width: 25%;" src="misc\images\Fries_Test.jpeg" alt="img">
+                    <div class="media-body">
+                        <div>
+                            <h3>Reviewers Name</h3>
+                            <h3>Stars</h3>
+                            <p>Full text review </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row border border-white rounded m-2">
+                <div class="media mx-3" style="padding-top: 15px; padding-bottom: 15px;">
+                    <img class="mr-3 align-self-center" style="height: auto; width: 25%;" src="misc\images\Fries_Test.jpeg" alt="img">
+                    <div class="media-body">
+                        <div>
+                            <h3>Reviewers Name</h3>
+                            <h3>Stars</h3>
+                            <p>Full text review </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row border border-white rounded m-2">
+                <div class="media mx-3" style="padding-top: 15px; padding-bottom: 15px;">
+                    <img class="mr-3 align-self-center" style="height: auto; width: 25%;" src="misc\images\Fries_Test.jpeg" alt="img">
+                    <div class="media-body">
+                        <div>
+                            <h3>Reviewers Name</h3>
+                            <h3>Stars</h3>
+                            <p>Full text review </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row border border-white rounded m-2">
+                <div class="media mx-3" style="padding-top: 15px; padding-bottom: 15px;">
+                    <img class="mr-3 align-self-center" style="height: auto; width: 25%;" src="misc\images\Fries_Test.jpeg" alt="img">
+                    <div class="media-body">
+                        <div>
+                            <h3>Reviewers Name</h3>
+                            <h3>Stars</h3>
+                            <p>Full text review </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
