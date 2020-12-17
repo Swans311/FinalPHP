f18823b95f7ca50ce0b290590656a7a50670a4f3
Swans311 <59575829+Swans311@users.noreply.github.com>
2020-12-17 11:32:20 -0500

    Add files via upload

--- NavBar.php
+++ NavBar.php
@@ -10,18 +10,16 @@ $loggedin = False
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap link-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <!-- Link to Style.css stylesheet-->
     <link rel="stylesheet" type="text/css" href="misc/css/Style.css">
-    <!-- Title not set to prevent overwriting each pages title-->
 </head>
 <body class="gz-body-bg"> 
 <nav class="navbar sticky-top navbar-expand-lg navbar-dark text-white shadow-lg p-3 mb-6 gz-nav-bg">
     <!-- Project Name/Logo-->
-    <a class="navbar-brand glow" href="NavBar.php" style="font-family: logoFont; font-size: 30px;">Gourmandize</a>
+    <a class="navbar-brand glow" href="homepage.php" style="font-family: logoFont; font-size: 30px;">Gourmandize</a>
       <!-- Nav links, pushed to left-->
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <a class="nav-link gz-nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link gz-nav-link" href="homepage.php">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link gz-nav-link" href="#">Review</a>
@@ -51,7 +49,7 @@ $loggedin = False
             <a class="nav-link gz-nav-link" href="#">Log Out</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link gz-nav-link" href="#"><img src="misc/img/user.svg" alt="Profile Icon" style="height: 24px; weight:24px;">  Account</a>
+            <a class="nav-link gz-nav-link" href="account.php"><img src="misc/img/user.svg" alt="Profile Icon" style="height: 24px; weight:24px;">  Account</a>
           </li>
         <?php endif ?>
       </ul>
